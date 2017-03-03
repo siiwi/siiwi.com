b8bc5fb8cdfd7b59afbd59e21f5da7d62855c3cf
李时海 <shihai.li@live.com>
2017-03-03 15:45:08 +0800

    https

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,12 +4,12 @@
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <meta name="Keywords" content="siiwi,siiwi.com,Saas,ERP,电商,电商管理工具,eBay,跨境电商,电子商务"/>
     <meta name="Description" content="Siiwi.com 做专业的电商管理工具，全心全意服务于中小企业。"/>
-    <link href="http://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="http://cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
-    <link href="http://cdn.bootcss.com/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css" rel="stylesheet" />
-    <link href="http://cdn.bootcss.com/sweetalert/1.1.3/sweetalert.min.css" rel="stylesheet" />
-    <link href="http://cdn.bootcss.com/pace/1.0.2/themes/blue/pace-theme-minimal.css" rel="stylesheet">
-    <link href="http://cdn.bootcss.com/lightgallery/1.2.14/css/lightgallery.min.css" rel="stylesheet">
+    <link href="//cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="//cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="//cdn.bootcss.com/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css" rel="stylesheet" />
+    <link href="//cdn.bootcss.com/sweetalert/1.1.3/sweetalert.min.css" rel="stylesheet" />
+    <link href="//cdn.bootcss.com/pace/1.0.2/themes/blue/pace-theme-minimal.css" rel="stylesheet">
+    <link href="//cdn.bootcss.com/lightgallery/1.2.14/css/lightgallery.min.css" rel="stylesheet">
     <link href="//cdn.bootcss.com/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.min.css" rel="stylesheet">
     <link href="{{ asset('css/fileinput.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/bootstrap-tagsinput.css') }}" rel="stylesheet" type="text/css" />
